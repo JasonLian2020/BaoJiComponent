7f07aef3d91bdd6d16853cb76f086a5f38bfd22b
jason <itlanguege@163.com>
2019-04-28 16:08:48 +0800

    Update MainActivity

--- app/src/main/java/com/baojiyule/baojicomponent/app/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/baojiyule/baojicomponent/app/mvp/ui/activity/MainActivity.java
@@ -1,15 +1,27 @@
 package com.baojiyule.baojicomponent.app.mvp.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.baojiyule.baojicomponent.app.R;
+import com.jess.arms.base.BaseActivity;
+import com.jess.arms.di.component.AppComponent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void setupActivityComponent(@NonNull AppComponent appComponent) {
+
+    }
+
+    @Override
+    public int initView(@Nullable Bundle savedInstanceState) {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+
     }
 }
