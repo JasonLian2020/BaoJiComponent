9ff93570ff7b4dba49a20fbf6f5435fc633f1746
jason <itlanguege@163.com>
2019-04-28 16:22:08 +0800

    Move Public Constants

--- CommonService/src/main/java/me/jessyan/armscomponent/commonservice/EventBusHub.java
+++ CommonService/src/main/java/me/jessyan/armscomponent/commonservice/EventBusHub.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package me.jessyan.armscomponent.commonsdk.core;
+package me.jessyan.armscomponent.commonservice;
 
 /**
  * ================================================
