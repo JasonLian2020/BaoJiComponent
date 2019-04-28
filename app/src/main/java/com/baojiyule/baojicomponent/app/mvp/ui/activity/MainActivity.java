7e90bd7f87ce5c4772f8b4c89ba49f73bed00d88
jason <itlanguege@163.com>
2019-04-28 16:51:32 +0800

    Update MainActivity

--- app/src/main/java/com/baojiyule/baojicomponent/app/mvp/ui/activity/MainActivity.java
+++ app/src/main/java/com/baojiyule/baojicomponent/app/mvp/ui/activity/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
 import com.baojiyule.baojicomponent.app.R;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 
+import me.jessyan.armscomponent.commonservice.RouterHub;
+
+@Route(path = RouterHub.APP_MAINACTIVITY)
 public class MainActivity extends BaseActivity {
 
     @Override
